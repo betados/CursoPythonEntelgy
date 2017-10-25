84fd12340cf6f19e59d305a4310b0fbb69d2f2e6
wueato03 <alvaro.torres@entelgy.com>
2017-10-25 14:00:38 +0200

    interaction between snake and food starting to work

--- snake.py
+++ snake.py
@@ -12,14 +12,13 @@ class Snake(object):
         self.vel = {'x': self.velMod, 'y': 0}
         self.lista = [{'x': 2, 'y': 2}]
         self.width = width
-        self.add()
+        # self.add()
         self.keyDict = {37: self.left, 38: self.up, 39: self.right, 40: self.down}
         self.estado = None
         self.keyDict[39]()
 
     def add(self):
-        for _ in range(4):
-            self.lista.append({'x': self.lista[-1]['x']-1, 'y': self.lista[-1]['y']})
+        self.lista.append({'x': self.lista[-1]['x']-1, 'y': self.lista[-1]['y']})
 
     def draw(self, canvas):
         for element in self.lista:
@@ -32,15 +31,6 @@ class Snake(object):
 
         self.lista[0]['x'] += self.vel['x']
         self.lista[0]['y'] += self.vel['y']
-
-
-
-    def getVel(self, p1, p2):
-        try:
-            dist = math.sqrt((math.pow(p2['x']-p1['x'], 2) + math.pow(p2['y']-p1['y'], 2)))
-            return {'x': (p2['x']-p1['x'])/dist, 'y': (p2['y']-p1['y'])/dist}
-        except:
-            return {'x': 0, 'y': 0}
 
     def left(self):
         if self.estado != "right":
@@ -61,6 +51,7 @@ class Snake(object):
         if self.estado != "up":
             self.vel = {'x': 0, 'y': self.velMod}
             self.estado = "down"
+
 
 class Square(object):
 
@@ -84,7 +75,7 @@ class Scenario(object):
         self.width = width
         self.height = height
         self.res = res
-        self.food = [(random.randrange(0, width, res), random.randrange(0, height, res)) for _ in range(6)]
+        self.food = [(random.randrange(0, width/res), random.randrange(0, height/res)) for _ in range(6)]
         self.lightGreen = [Square((x+res*(y % (res*2) == 0), y), res, 'GreenYellow ') for x in range(width)
                            if x % (res*2) == 0 for y in range(height) if y % res == 0]
         self.backGround = Square((0, 0), width, 'DarkKhaki')
@@ -94,7 +85,15 @@ class Scenario(object):
         for square in self.lightGreen:
             square.draw(canvas)
         for apple in self.food:
-            canvas.draw_circle((apple[0]+self.res/2, apple[1]+self.res/2), self.res/2 * 0.7, 1, 'Red', 'Red')
+            canvas.draw_circle(((apple[0]+0.5)*self.res, (apple[1]+0.5)*self.res), self.res/2 * 0.7, 1, 'Red', 'Red')
+
+
+class Interaction(object):
+    # @staticmethod
+    def food(self, snake, scenario):
+        for apple in scenario.food:
+            if snake.lista[0]['x'] == apple[0] and snake.lista[0]['y'] == apple[1]:
+                snake.add()
 
 
 # Handler for mouse click
@@ -121,12 +120,14 @@ def draw(canvas):
 
 def actualize():
     snake.actualize()
+    interaction.food(snake, scenario)
 
 
 width = 320
 height = 240
 snake = Snake(20)
 scenario = Scenario(width, height, 20)
+interaction = Interaction()
 
 # Create a frame and assign callbacks to event handlers
 frame = simplegui.create_frame("Home", width, height)
